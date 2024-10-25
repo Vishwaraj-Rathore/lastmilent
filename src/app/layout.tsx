0a3cd71e5c5fa0c587e2021f187ff02d51775274
Vishwaraj Rathore <53338624+Vishwaraj-Rathore@users.noreply.github.com>
2024-10-25 21:24:17 +0530

    fixed some minor issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
-import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
-import "./globals.css";
-
-export const metadata = {
-  title: "Last Mile Enterprises",
-  description: "Under Construction ...",
-};
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function RootLayout({
   children,
@@ -16,7 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NavBar />
-        {children}
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
