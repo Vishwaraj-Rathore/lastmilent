852a4fdc2479480e31fc223197ac4f59923766b1
Vishwaraj Rathore <53338624+Vishwaraj-Rathore@users.noreply.github.com>
2024-10-26 10:53:22 +0530

    small changes

---
+++ src/app/fairlane/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Fair Lane</div>;
+};
+
+export default page;
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -7,13 +7,21 @@ const NavBar = () => {
   return (
     <>
       <nav className="p-5 flex bg-white justify-between items-center">
-        <LogoButton />
-        <ul>
+        <div className="pl-2">
+          <LogoButton />
+        </div>
+        <ul className="hidden md:flex gap-12 pr-7">
           <li className="font-medium hover:text-blue-700">
             <LinkButton title={"Home"} location={"/"} />
           </li>
           <li className="font-medium hover:text-blue-700">
             <LinkButton title={"SunBless"} location={"/sunbless"} />
+          </li>
+          <li className="font-medium hover:text-blue-700">
+            <LinkButton title={"Fair Lane"} location={"/fairlane"} />
+          </li>
+          <li className="font-medium hover:text-blue-700">
+            <LinkButton title={"Just Corseca"} location={"/justcorseca"} />
           </li>
           <li className="font-medium hover:text-blue-700">
             <LinkButton title={"Contact Us"} location={"/contact"} />
